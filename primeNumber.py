6f1609721cfc74ee24c654c55168a6c6964597f3
XamMushki <xahidmushki13@gmail.com>
2023-06-04 00:17:50 +0530

    Prime Number using for loop and while loop

--- primeNumber.py
+++ primeNumber.py
@@ -1,15 +1,34 @@
-num = int(input('Enter Number: '))
-if num == 1:
-    print(num, 'is not a prime number.')
-elif num > 1:
-    i = 2
-    flag = True
-    while i < num:
-        if num % i == 0:
-            flag = False
-            break
-        i += 1
-    if flag == True:
-        print(num, 'is a prime number.')
-    else:
+def primeUsingWhile(num):
+    if num == 1:
         print(num, 'is not a prime number.')
+    elif num > 1:
+        i = 2
+        flag = True
+        while i < num:
+            if num % i == 0:
+                flag = False
+                break
+            i += 1
+        if flag == True:
+            print(num, 'is a prime number.')
+        else:
+            print(num, 'is not a prime number.')
+
+
+def primeUsingFor(num):
+    if num == 1:
+        print(num, 'is not a prime number.')
+    elif num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                print(num, 'is not a prime number.')
+                break
+        else:
+            # executes only if the break statement in for loop does not execute
+            print(num, 'is a prime number.')
+
+
+if __name__ == '__main__':
+    num = int(input('Enter Number: '))
+    primeUsingWhile(num)
+    primeUsingFor(num)
